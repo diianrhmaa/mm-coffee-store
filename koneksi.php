b8149e78d5cd5166beec4643d38fedcc2cca434d
diianrhmaa <diianrhma06@gmail.com>
2023-06-27 08:11:12 +0700

    initial commit

---
+++ koneksi.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost'; // nama host database
+$dbname = 'newcoffee_shop'; // nama database
+$username = 'root'; // nama pengguna database
+$password = ''; // kata sandi database
+$koneksi = mysqli_connect("localhost", "root", "", "newcoffee_shop");
+
+try {
+    // membuat koneksi ke database
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $dbh = new PDO($dsn, $username, $password);
+
+    // Set error mode to exception
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Koneksi ke database berhasil ->" .;
+} catch (PDOException $e) {
+    echo "Koneksi ke database gagal: " . $e->getMessage();
+}
+?>
